f03582a6e33eabe7a62de40c069df6ace8222766
Terry_ Stannard <114263990+Terry0101Stannard@users.noreply.github.com>
2024-03-01 11:11:13 +0000

    Some changes
    Cyber page cleaned up
    
    Homepage started

--- assets/js/main.js
+++ assets/js/main.js
@@ -209,3 +209,19 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 });
+
+function EasyPeasyParallax() {
+	scrollPos = $(this).scrollTop();
+	$('#banner').css({
+		'background-position' : '50% ' + (-scrollPos/4)+"px"
+	});
+	$('#bannertext').css({
+		'margin-top': (scrollPos/4)+"px",
+		'opacity': 1-(scrollPos/250)
+	});
+}
+$(document).ready(function(){
+	$(window).scroll(function() {
+		EasyPeasyParallax();
+	});
+});
